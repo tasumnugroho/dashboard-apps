3e35b5fb318b08e02129d4b5c850cb6966028955
gatotnugroho <46276556+tasumnugroho@users.noreply.github.com>
2023-10-24 14:30:47 +0000

    datasetCode

--- Dashboard.py
+++ Dashboard.py
@@ -7,25 +7,26 @@ sns.set(style='dark')
 
 
 def create_daily_orders_df(df):
-    daily_orders_df = df.resample(rule='D', on='order_date').agg({
+    daily_orders_df = df.resample(rule='D', on='order_purchase_timestamp').agg({
         "order_id": "nunique",
-        "total_price": "sum"
+        "price": "sum"
     })
     daily_orders_df = daily_orders_df.reset_index()
     daily_orders_df.rename(columns={
         "order_id": "order_count",
-        "total_price": "revenue"
+        "price": "revenue"
     }, inplace=True)
     
     return daily_orders_df
 
 
 def create_sum_order_items_df(df):
-    sum_order_items_df = df.groupby("product_name").quantity_x.sum().sort_values(ascending=False).reset_index()
+    sum_order_items_df = df.groupby("product_id").order_item_id.count().sort_values(ascending=False).reset_index()
     return sum_order_items_df
 
+
 def create_bygender_df(df):
-    bygender_df = df.groupby(by="gender").customer_id.nunique().reset_index()
+    bygender_df = df.groupby(by="customer_zip_code_prefix").customer_id.nunique().reset_index()
     bygender_df.rename(columns={
         "customer_id": "customer_count"
     }, inplace=True)
@@ -33,18 +34,20 @@ def create_bygender_df(df):
     return bygender_df
 
 
-def create_byage_df(df):
-    byage_df = df.groupby(by="age_group").customer_id.nunique().reset_index()
-    byage_df.rename(columns={
-        "customer_id": "customer_count"
-    }, inplace=True)
-    byage_df['age_group'] = pd.Categorical(byage_df['age_group'], ["Youth", "Adults", "Seniors"])
+
+#def create_byage_df(df):
+    #byage_df = df.groupby(by="customer_city").customer_id.nunique().reset_index()
+    #byage_df.rename(columns={
+    #"customer_id": "customer_count"
+    #}, inplace=True)
+    #byage_df['age_group'] = pd.Categorical(byage_df['age_group'], ["Youth", "Adults", "Seniors"])
     
-    return byage_df
+    #return byage_df
+
 
 
 def create_bystate_df(df):
-    bystate_df = df.groupby(by="state").customer_id.nunique().reset_index()
+    bystate_df = df.groupby(by="customer_state").customer_id.nunique().reset_index()
     bystate_df.rename(columns={
         "customer_id": "customer_count"
     }, inplace=True)
@@ -54,17 +57,54 @@ def create_bystate_df(df):
 
 def create_rfm_df(df):
     rfm_df = df.groupby(by="customer_id", as_index=False).agg({
-        "order_date": "max", #mengambil tanggal order terakhir
+        "order_purchase_timestamp": "max", #mengambil tanggal order terakhir
         "order_id": "nunique",
-        "total_price": "sum"
+        "price": "sum"
     })
     rfm_df.columns = ["customer_id", "max_order_timestamp", "frequency", "monetary"]
     
     rfm_df["max_order_timestamp"] = rfm_df["max_order_timestamp"].dt.date
-    recent_date = df["order_date"].dt.date.max()
+    recent_date = df["order_purchase_timestamp"].dt.date.max()
     rfm_df["recency"] = rfm_df["max_order_timestamp"].apply(lambda x: (recent_date - x).days)
     rfm_df.drop("max_order_timestamp", axis=1, inplace=True)
     
     return rfm_df
 
-all_df = pd.read_csv("all_data.csv")
+all_df = pd.read_csv("alldata_df.csv")
+
+
+datetime_columns = ["order_purchase_timestamp", "order_delivered_customer_date"]
+all_df.sort_values(by="order_purchase_timestamp", inplace=True)
+all_df.reset_index(inplace=True)
+ 
+for column in datetime_columns:
+    all_df[column] = pd.to_datetime(all_df[column])
+
+#---#
+
+min_date = all_df["order_purchase_timestamp"].min()
+max_date = all_df["order_purchase_timestamp"].max()
+ 
+with st.sidebar:
+    # Menambahkan logo perusahaan
+    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
+    
+    # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+
+main_df = all_df[(all_df["order_purchase_timestamp"] >= str(start_date)) & 
+                (all_df["order_purchase_timestamp"] <= str(end_date))]
+
+
+daily_orders_df = create_daily_orders_df(main_df)
+sum_order_items_df = create_sum_order_items_df(main_df)
+bygender_df = create_bygender_df(main_df)
+#byage_df = create_byage_df(main_df)
+bystate_df = create_bystate_df(main_df)
+rfm_df = create_rfm_df(main_df)
+
+st.header('Dicoding Collection Dashboard :sparkles:')
